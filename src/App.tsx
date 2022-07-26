548b69d77842f32c8591dad260e98d8f345aeedb
fortdev210 <sharp.blader2050@gmail.com>
2022-07-26 17:19:48 +0800

    onclick outside close modal

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,10 @@ function App() {
 
   return (
     <div className=" bg-gray-100  h-screen  p-2 ">
-      {!open && (
-        <button className="btn btn-primary" onClick={() => setOpen(true)}>
-          Open Modal
-        </button>
-      )}
-
-      {open && <VerificationModal open={open} onOpenChange={() => setOpen(false)} />}
-
-      {open && (
-        <div
-          className="fixed inset-0  bg-black bg-opacity-40  z-10"
-          onClick={() => setOpen(false)}
-        />
-      )}
+      <button className="btn btn-primary" onClick={() => setOpen(true)}>
+        Open Modal
+      </button>
+      <VerificationModal open={open} onOpenChange={() => setOpen(false)} />
     </div>
   )
 }
--- src/components/VerificationModal.tsx
+++ src/components/VerificationModal.tsx
@@ -13,6 +13,8 @@ export interface VerificationModalProps {
 }
 
 const BACKSPACE_KEY = 'Backspace'
+const LEFT_KEY = 'ArrowLeft'
+const RIGHT_KEY = 'ArrowRight'
 const CODE_LENGTH = 5
 
 type InputType = number | null
@@ -39,12 +41,23 @@ export const VerificationModal: React.FC<VerificationModalProps> = ({
 
   const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>, index: number) => {
     const key = event.nativeEvent.code
-    if (key !== BACKSPACE_KEY) {
-      return
+
+    switch (key) {
+      case BACKSPACE_KEY:
+        event.preventDefault()
+        onInputChange(null, index)
+        changeInputFocus(index - 1)
+        break
+      case RIGHT_KEY:
+        changeInputFocus(index + 1)
+        break
+      case LEFT_KEY:
+        changeInputFocus(index - 1)
+        break
+
+      default:
+        return
     }
-    event.preventDefault()
-    onInputChange(null, index)
-    changeInputFocus(index - 1)
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
@@ -65,41 +78,51 @@ export const VerificationModal: React.FC<VerificationModalProps> = ({
     }
   }
 
-  return open ? (
-    <div className="relative m-auto px-8 py-10 bg-white  max-w-xl  z-20  transform translate-y-1/2 rounded-lg shadow-md">
-      <p className="text-gray-700  font-bold text-2xl">Confirm your number</p>
-      <p className="mt-4">We have sent you a code to your phone number (647)-123-***</p>
-      <div className="flex  flex-wrap mt-12 space-x-2 sm:space-x-4">
-        {new Array(CODE_LENGTH).fill(0).map((_, i) => (
-          <input
-            className="w-12 h-12  sm:text-xl mt-2 sm:w-16 sm:h-16 border border-gray-200 rounded-lg p-4 sm:p-6 hover:border-blue-500 focus:border-blue-500 focus:outline-none"
-            key={i}
-            type="number"
-            ref={(el) => {
-              if (el) {
-                inputRefs.current[i] = el
+  return (
+    <>
+      {open ? (
+        <div className="relative m-auto px-8 py-10 bg-white  max-w-xl  z-20  transform translate-y-1/2 rounded-lg shadow-md">
+          <p className="text-gray-700  font-bold text-2xl">Confirm your number</p>
+          <p className="mt-4">We have sent you a code to your phone number (647)-123-***</p>
+          <div className="flex  flex-wrap mt-12 space-x-2 sm:space-x-4">
+            {new Array(CODE_LENGTH).fill(0).map((_, i) => (
+              <input
+                className="w-12 h-12  sm:text-xl mt-2 sm:w-16 sm:h-16 border border-gray-200 rounded-lg p-4 sm:p-6 hover:border-blue-500 focus:border-blue-500 focus:outline-none"
+                key={i}
+                type="number"
+                ref={(el) => {
+                  if (el) {
+                    inputRefs.current[i] = el
+                  }
+                }}
+                onKeyDown={(e) => onKeyDown(e, i)}
+                value={code[i] || ''}
+                onChange={(e) => onChange(e, i)}
+                onPaste={onPasteNumber}
+              ></input>
+            ))}
+          </div>
+          <div className="flex mt-6 space-x-2">
+            <p>Didn't receive a code?</p>
+            <span className=" text-blue-500  cursor-pointer">Resend</span>
+          </div>
+          <style>
+            {`
+              input::-webkit-outer-spin-button,
+              input::-webkit-inner-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
               }
-            }}
-            onKeyDown={(e) => onKeyDown(e, i)}
-            value={code[i] || ''}
-            onChange={(e) => onChange(e, i)}
-            onPaste={onPasteNumber}
-          ></input>
-        ))}
-      </div>
-      <div className="flex mt-6 space-x-2">
-        <p>Didn't receive a code?</p>
-        <span className=" text-blue-500  cursor-pointer">Resend</span>
-      </div>
-      <style>
-        {`
-          input::-webkit-outer-spin-button,
-          input::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-          }
-        `}
-      </style>
-    </div>
-  ) : null
+            `}
+          </style>
+        </div>
+      ) : null}
+      {open && (
+        <div
+          className="fixed inset-0  bg-black bg-opacity-40  z-10"
+          onClick={() => onOpenChange(false)}
+        />
+      )}
+    </>
+  )
 }
